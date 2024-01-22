6e425fc6f7995aefd8217a75b9988aa88a04ad18
antri <akrb2204@gmail.com>
2024-01-22 13:05:08 +0530

    nav update

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -15,13 +15,19 @@ const Navbar = () => {
       <nav className="flex lg:justify-between justify-center p-5">
         <div className="logo hidden lg:block ">
           <Link href="/">
-            <Image src="logo.svg" alt="Logo" width={150} height={150} className="invert"/>
+            <Image
+              src="logo.svg"
+              alt="Logo"
+              width={150}
+              height={150}
+              className="invert"
+            />
           </Link>
         </div>
         <div className="nav-items flex space-x-5 items-center px-2">
           <SearchBar />
-          <UserChat />
           <AddListing />
+          <UserChat />
           <SavedItems />
           {/* <ModeToggle /> */}
           <Separator orientation="vertical" />
