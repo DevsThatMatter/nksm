5ace9f069a7313b9bee82a5fe57c54accae133bb
antri <akrb2204@gmail.com>
2024-01-22 11:17:07 +0530

    category : css only implementation

--- components/widgets/categoryCard.tsx
+++ components/widgets/categoryCard.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
 
 interface CategoryProp {
-    name: string,
-    imgUrl: string,
-    width?: string,
-    height: string
+  name: string;
+  imgUrl: string;
+  width?: string;
+  height?: string;
+  className?: string;
 }
-const CategoryCard: React.FC<CategoryProp> = ({name, imgUrl, width, height}) => {
+const CategoryCard: React.FC<CategoryProp> = ({ name, imgUrl, className }) => {
   return (
-    <Link href={""} key={name} className="lg:w-[50%]">
-      <div className="flex justify-center rounded-3xl p-2" >
-        <img
-          src={imgUrl}
-          alt={name}
-          className={`rounded-lg object-cover hover:opacity-75`}
-          style={{ width: "100%", height: height }}
-        />
-      </div>
+    <Link
+      href={""}
+      key={name}
+      className={`${className}  rounded-3xl p-2 md:h-[60vh] sm:h-[30vh] lg:h-auto`}
+    >
+      <img
+        src={imgUrl}
+        alt={name}
+        className={`rounded-lg object-cover hover:opacity-75`}
+        style={{ width: "100%", height: "100%" }}
+      />
     </Link>
   );
 };
