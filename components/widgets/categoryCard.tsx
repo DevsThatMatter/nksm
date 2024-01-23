8b4de1e50e834c23083f16080ef40e2eb6663834
antri <akrb2204@gmail.com>
2024-01-23 21:17:28 +0530

    fix2?

--- components/widgets/categoryCard.tsx
+++ components/widgets/categoryCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { spartan } from "@/app/ui/fonts";
+import { cn } from "@/lib/utils";
 interface CategoryProp {
   name: string;
   imgUrl: string;
@@ -19,18 +20,28 @@ const CategoryCard: React.FC<CategoryProp> = ({
     <Link
       href={""}
       key={name}
-      className={`@container relative ${className}  rounded-3xl p-2 sm:h-[30vh] md:h-[60vh] lg:h-auto overflow-hidden `}
+      className={cn(
+        "@container relative rounded-3xl p-2 sm:h-[30vh] md:h-[60vh] lg:h-auto overflow-hidden",
+        className,
+      )}
     >
       <Image
         src={imgUrl}
         alt={name}
-        className={` rounded-lg object-cover hover:opacity-75 h-full w-full ${imageClassName}`}
+        className={cn(
+          "rounded-lg object-cover hover:opacity-75 h-full w-full ",
+          imageClassName,
+        )}
         width={800}
         height={800}
       />
 
       <h1
-        className={`absolute w-fit h-fit text-2xl @2xs:text-3xl @xs:text-4xl @sm:text-5xl @md:text-6xl font-semibold @md:font-medium text-gray-700 ${textClassName} ${spartan.className}`}
+        className={cn(
+          "absolute w-fit h-fit text-2xl @2xs:text-3xl @xs:text-4xl @sm:text-5xl @md:text-6xl font-semibold @md:font-medium text-gray-700",
+          textClassName,
+          spartan.className,
+        )}
       >
         {name}
       </h1>
