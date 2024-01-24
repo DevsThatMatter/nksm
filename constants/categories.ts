8e873d10b62c310f4661f18d1dd35783d46d0e43
antri <akrb2204@gmail.com>
2024-01-25 02:05:23 +0530

    images

--- constants/categories.ts
+++ constants/categories.ts
@@ -2,50 +2,50 @@ import { category } from "@/types";
 export const categories: category[] = [
   {
     name: "Bicycles",
-    imgUrl: "/categoriesImagesNoTxt/Bicycle.png",
+    imgUrl: "/images/Categories/Bicycle.png",
     className: "lg:row-span-2",
     textClassName: "top-[20%] right-0 left-0 mx-auto",
   },
   {
     name: "Coolers",
-    imgUrl: "/categoriesImagesNoTxt/Cooler.png",
+    imgUrl: "/images/Categories/Cooler.png",
     imageClassName: "object-left-top",
     className: "lg:col-span-2 lg:row-span-3",
     textClassName: "top-[20%] left-[15%]",
   },
   {
     name: "Stationery",
-    imgUrl: "/categoriesImagesNoTxt/Stationery.jpg",
+    imgUrl: "/images/Categories/Stationery.jpg",
     textClassName:
       "top-[20%]  pl-3 right-0 left-0 mx-auto top-[25%] xl:top-[20%]",
   },
   {
     name: "Miscellaneous",
-    imgUrl: "/categoriesImagesNoTxt/Miscellaneous.jpg",
+    imgUrl: "/images/Categories/Miscellaneous.jpg",
     className: "lg:row-span-2",
     textClassName: "top-[10%] left-[5%] @md:top-[15%]",
   },
 
   {
     name: "Mattresses",
-    imgUrl: "/categoriesImagesNoTxt/Mattress.png",
+    imgUrl: "/images/Categories/Mattress.png",
     className: "lg:row-span-2",
     textClassName: "inset-0 m-auto",
   },
   {
     name: "Kitchenware",
-    imgUrl: "/categoriesImagesNoTxt/Kettle.png",
+    imgUrl: "/images/Categories/Kettle.png",
     imageClassName: "object-right-top",
     textClassName: "top-[20%] sm:right-[20%] right-[5%]",
   },
   {
     name: "Instruments",
-    imgUrl: "/categoriesImagesNoTxt/Instruments.jpg",
+    imgUrl: "/images/Categories/Instruments.jpg",
     textClassName: "top-[15%] left-[10%]",
   },
   {
     name: "Electronics",
-    imgUrl: "/categoriesImagesNoTxt/Electronics.png",
+    imgUrl: "/images/Categories/Electronics.png",
     textClassName: "top-[15%] left-[10%]",
   },
 ];
