7ef4367d498ef56ee16f60f749c060951b5d4419
Aryan Srivastava <75990590+aryansri-19@users.noreply.github.com>
2024-01-24 12:44:08 +0000

    Final socket push

--- components/Chat/userChat.tsx
+++ components/Chat/userChat.tsx
@@ -2,12 +2,12 @@
 // bits
 
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
 } from "../ui/sheet";
 import { Button } from "../ui/button";
 import useChatStore from "../../Hooks/chatStore";
@@ -15,8 +15,8 @@ import ChatUI from "./ChatUI";
 import { Icons } from "@/app/ui/icons";
 
 interface Chat {
-  name: string;
-  phoneNumber: string;
+    name: string;
+    phoneNumber: string;
 }
 
 export default function UserChat() {
@@ -35,63 +35,62 @@ export default function UserChat() {
             console.error(error);
         }
     }
-  }
 
-  // Dummy data
-  const chats: Chat[] = [{ name: "Bits", phoneNumber: "+91-1234567800" }];
-    return (
-        <div>
-            <Sheet>
-                <SheetTrigger asChild >
-                    <Button variant="ghost">
-                        <Icons.chaticon />
-                    </Button>
-                </SheetTrigger>
-                {chats ? (
-                    <SheetContent side="left">
-                        <SheetHeader>
-                            <SheetTitle>
-                                {chat === "" ? (
-                                    <span>Lets have some chats...</span>
-                                ) : (
-                                    <Button
-                                        className="-left-4"
-                                        size={"icon"}
-                                        variant="ghost"
-                                        onClick={() => {
-                                            removeChat();
-                                        }}
-                                    >
-                                        <Icons.moveback />
-                                    </Button>
-                                )}
-                            </SheetTitle>
-                        </SheetHeader>
-                        {chat === "" ? (
-                            <SheetDescription className="mt-4 flex flex-col space-y-4 w-full">
-                                {chats.map((chat) => (
-                                    <div
-                                        key={chat.phoneNumber}
-                                        className="flex items-center justify-between p-2 border rounded-md shadow-md cursor-pointer hover:shadow-xl"
-                                        onClick={() => handleSetChat(chat.phoneNumber)}
-                                    >
-                                        <div>
-                                            <h1 className="text-lg font-bold">{chat.name}</h1>
-                                            <p className="text-md font-medium">{chat.phoneNumber}</p>
-                                        </div>
-                                        {/* Add any additional information like last message or online status here */}
+// Dummy data
+const chats: Chat[] = [{ name: "Bits", phoneNumber: "+91-1234567800" }];
+return (
+    <div>
+        <Sheet>
+            <SheetTrigger asChild >
+                <Button variant="ghost">
+                    <Icons.chaticon className="w-[1.32rem] h-[1.3rem]"/>
+                </Button>
+            </SheetTrigger>
+            {chats ? (
+                <SheetContent side="left">
+                    <SheetHeader>
+                        <SheetTitle>
+                            {chat === "" ? (
+                                <span>Lets have some chats...</span>
+                            ) : (
+                                <Button
+                                    className="-left-4"
+                                    size={"icon"}
+                                    variant="ghost"
+                                    onClick={() => {
+                                        removeChat();
+                                    }}
+                                >
+                                    <Icons.moveback />
+                                </Button>
+                            )}
+                        </SheetTitle>
+                    </SheetHeader>
+                    {chat === "" ? (
+                        <SheetDescription className="mt-4 flex flex-col space-y-4 w-full">
+                            {chats.map((chat) => (
+                                <div
+                                    key={chat.phoneNumber}
+                                    className="flex items-center justify-between p-2 border rounded-md shadow-md cursor-pointer hover:shadow-xl"
+                                    onClick={() => handleSetChat(chat.phoneNumber)}
+                                >
+                                    <div>
+                                        <h1 className="text-lg font-bold">{chat.name}</h1>
+                                        <p className="text-md font-medium">{chat.phoneNumber}</p>
                                     </div>
-                                ))}
-                            </SheetDescription>
-                        ) : (
-                            <ChatUI />
-                        )}
-                    </SheetContent>
-                ) : (
-                    <div>{/* UI for no chats */}</div>
-                )}
-            </Sheet>
-        </div>
+                                    {/* Add any additional information like last message or online status here */}
+                                </div>
+                            ))}
+                        </SheetDescription>
+                    ) : (
+                        <ChatUI />
+                    )}
+                </SheetContent>
+            ) : (
+                <div>{/* UI for no chats */}</div>
+            )}
+        </Sheet>
+    </div>
 
-    );
+);
 }
