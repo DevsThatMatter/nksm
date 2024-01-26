9fab713cd86c16826ead4a3c0de5065750d8b8ac
geekysilento <adityakarna.ak@gmail.com>
2024-01-26 21:20:51 +0545

    bug: fixed categories padding n navbar icon size

--- app/components/Navbar/SavedItems.tsx
+++ app/components/Navbar/SavedItems.tsx
@@ -3,7 +3,7 @@ import { Icons } from "@/app/utils/icons";
 
 export function SavedItems() {
   return (
-    <Button variant="ghost" size="icon">
+    <Button variant="ghost" size="default">
       <Icons.saved className="h-[1.4rem] w-[1.4rem] " />
     </Button>
   );
