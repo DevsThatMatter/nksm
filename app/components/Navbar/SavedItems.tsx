265351c248e5f72c3f8c47b3b9b54a9375d44afc
geekysilento <adityakarna.ak@gmail.com>
2024-01-27 00:41:27 +0545

    navbar icon fix

--- app/components/Navbar/SavedItems.tsx
+++ app/components/Navbar/SavedItems.tsx
@@ -3,7 +3,7 @@ import { Icons } from "@/app/utils/icons";
 
 export function SavedItems() {
   return (
-    <Button variant="ghost" size="default">
+    <Button variant="ghost" size="icon">
       <Icons.saved className="h-[1.4rem] w-[1.4rem] " />
     </Button>
   );
