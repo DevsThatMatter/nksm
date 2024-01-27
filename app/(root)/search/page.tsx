db241cfca87564d9b9f666fc50114d4270915c0c
geekysilento <125768789+geekysilento@users.noreply.github.com>
2024-01-27 21:12:59 +0545

    Working frontend logic for searchbar, added a basic search page aswell. (#30)
    * bug: fixed categories padding n navbar icon size
    
    * navbar icon fix
    
    * Carousel add
    
    * Changes
    
    * added search dropdown ui
    
    * working search bar and added basic search page
    
    * dark mode fix
    
    * better visibility in dark mode
    
    * carousel bugfix
    
    * Minor alignment
    
    ---------
    
    Co-authored-by: aryansri-19 <aryansri.19@gmail.com>

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,11 @@
+export default function Page({
+    searchParams,
+  }: {
+    searchParams: { [key: string]: string | undefined };
+  }) {
+    const valuesString = Object.values(searchParams).join(', ');
+
+    return (
+      <div>Showing related products for {valuesString}</div>
+    );
+  }
