da08b4b5d2f6e620a934996f3d7ce1427aac661b
geekysilento <adityakarna.ak@gmail.com>
2024-01-27 21:32:54 +0545

    added logic for navigating multipage form

--- app/components/AddListing/index.tsx
+++ app/components/AddListing/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from 'react';
 import { Button } from "@/app/components/ui/button";
 import { Icons } from "@/app/utils/icons";
 import {
@@ -13,45 +15,51 @@ import { Input } from "@/app/components/ui/input";
 import { Label } from "@/app/components/ui/label";
 import { StepOne } from "./StepOne";
 import { StepTwo } from "./StepTwo";
+import { StepThree } from "./StepThree";
+import { StepFour } from "./StepFour"; // Assuming you have a StepFour component
 
 export function AddListing() {
+  const [currentStep, setCurrentStep] = useState<number>(1);
 
+  const handleNext = () => {
+    // Increment current step when "Next" is clicked
+    setCurrentStep(currentStep + 1);
+  };
+
+  // Define the content for each step
+  const stepContent: Record<number, JSX.Element> = {
+    1: <StepOne />,
+    2: <StepTwo />,
+    3: <StepThree />,
+    // 4: <StepFour />,
+  };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="default" className="relative">
-          <Icons.add className="absolute  left-2 m-auto top-0 bottom-0 h-5 w-5" />
+          <Icons.add className="absolute left-2 m-auto top-0 bottom-0 h-5 w-5" />
           <span className="hidden sm:inline-block pl-4"> Add Listing </span>
         </Button>
       </DialogTrigger>
       <div className="grid gap-4">
-
-      <DialogContent className="l:max-w-[800px] h-[600px] grid gap-5">
+        <DialogContent className="l:max-w-[800px] h-[600px] grid gap-5">
           <DialogHeader>
-
-            <div> 
-
-              step 1 of 4.</div>
+            <div>{`Step ${currentStep} of 4.`}</div>
             <DialogTitle>Add Listing</DialogTitle>
             <DialogDescription>
               Provide details for your new listing.
             </DialogDescription>
           </DialogHeader>
-            
-            
           <div className="bg-gray-50 dark:bg-gray-950 p-6 rounded-lg shadow-2xl">
-            <StepTwo />
+            {stepContent[currentStep]}
           </div>
-
-          
-
-
-          <DialogFooter className="flex justify-end mt-auto">
-            <Button type="submit">
-              Next
-            </Button> 
-          
+          <DialogFooter className="flex mt-auto">
+            {currentStep < 4 && (
+              <Button type="button" onClick={handleNext}>
+                Next
+              </Button>
+            )}
           </DialogFooter>
         </DialogContent>
       </div>
