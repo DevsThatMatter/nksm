839ad5df59ebb1e4b1d5c1a8fa724c2b3ca23260
Aryan Srivastava <75990590+aryansri-19@users.noreply.github.com>
2024-01-27 20:38:51 +0530

    Carousel add (#28)
    * Carousel add
    
    * Changes

---
+++ app/components/HomePage/Carousel/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/app/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { EmblaOptionsType, EmblaPluginType } from "embla-carousel";
+
+import ProductCard from "@/app/components/HomePage/Carousel/ProductCard";
+import { products } from '@/constants/products'
+
+const options: EmblaOptionsType = { loop: true, align: "center" };
+const plugins: EmblaPluginType[] = [Autoplay({ delay: 5000 })];
+const ProductCarousel = () => {
+  return (
+    <>
+      <div className="lg:m-9 lg:mt-12 p-4">
+        <h1 className="text-2xl font-semibold">Recent Items</h1>
+        <Carousel className="w-full" opts={options} plugins={plugins}>
+          <CarouselContent>
+            {
+            products.map((product)=> (
+              <CarouselItem
+                key={product.name}
+                className="lg:basis-1/4 basis-1/2 md:basis-1/3 xl:basis-1/5 xs:basis-1/3"
+              >
+                <ProductCard
+                  image_url={product.image_url}
+                  name={product.name}
+                  price={product.price}
+                  description={product.description}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden lg:flex"/>
+          <CarouselNext className="hidden lg:flex"/>
+        </Carousel>
+      </div>
+    </>
+  );
+};
+
+export default ProductCarousel;
---
+++ constants/products.ts
@@ -0,0 +1,58 @@
+export const products = [
+    {
+        image_url: "/Images/Categories/Bicycle.png",
+        name: "Bicycle",
+        price: 100,
+        description:
+          "A bicycle is a human-powered or motor-powered, pedal-driven, single-track vehicle, having two wheels attached to a frame, one behind the other. A bicycle rider is called a cyclist, or bicyclist.",
+    },
+    {
+        image_url: "/Images/Categories/Cooler.png",
+        name: "Cooler",
+        price: 100,
+        description:
+          "A cooler, portable ice chest, ice box, cool box, chilly bin (in New Zealand), or esky (Australia) is an insulated box used to keep food or drink cool.",
+    },
+    {
+        image_url: "/Images/Categories/Stationery.jpg",
+        name: "Stationery",
+        price: 100,
+        description:
+          "Stationery is a mass noun referring to commercially manufactured writing materials, including cut paper, envelopes, writing implements, continuous form paper, and other office supplies.",
+    },
+    {
+        image_url: "/Images/Categories/Miscellaneous.jpg",
+        name: "Miscellaneousasfasdfasfafsdfsdasafsadadafasdfasdasd",
+        price: 100,
+        description:
+          "Miscellaneous is the adjective form of the noun miscellany, which means a mixture of different things.",
+    },
+    {
+        image_url: "/Images/Categories/Mattress.png",
+        name: "Mattress",
+        price: 100,
+        description:
+          "A mattress is a large, rectangular pad for supporting the reclining body, designed to be used as a bed or on a bed frame, as part of a bed.",
+    },
+    {
+        image_url: "/Images/Categories/Kettle.png",
+        name: "Kettle",
+        price: 100,
+        description:
+          "A kettle, sometimes called a tea kettle or teakettle, is a type of pot specialized for boiling water, with a lid, spout, and handle, or a small kitchen appliance of similar shape that functions in a self-contained manner.",
+    },
+    {
+        image_url: "/Images/Categories/Instruments.jpg",
+        name: "Instruments",
+        price: 100,
+        description:
+          "A musical instrument is a device created or adapted to make musical sounds. In principle, any object that produces sound can be considered a musical instrument—it is through purpose that the object becomes a musical instrument.",
+    },
+    {
+        image_url: "/Images/Categories/Electronics.png",
+        name: "Electronics",
+        price: 100,
+        description:
+          "Electronics comprises the physics, engineering, technology and applications that deal with the emission, flow and control of electrons in vacuum and matter.",
+    },
+]
