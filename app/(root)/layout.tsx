b2f81bf41add6ad1995e240a6ad521d13076ce2e
geekysilento <adityakarna.ak@gmail.com>
2024-02-01 01:47:21 +0545

    added auth page and initialized auth.js

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/app/components/Navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-background">
       <Navbar />
       {children}
     </main>
--- app/components/Auth/Login/index.tsx
+++ app/components/Auth/Login/index.tsx
@@ -1,6 +1,19 @@
 "use client";
+
+import { Button } from "../../ui/button";
+import { CardWrapper } from "../card-wrapper";
+import { FcGoogle } from 'react-icons/fc'
 const LoginForm = () => {
-  return <div></div>;
+  return <CardWrapper
+    headerLabel="Welcome to NKSM!"
+    backButtonLabel="Don't have an account?"
+    backButtonHref="/signup"
+  >
+    <Button size="lg" className="w-full flex items-center justify-center space-x-3" variant='outline' onClick= {() => {}} >
+      <FcGoogle className="h-6 w-6"/>
+      <span>Continue with Google</span>
+    </Button>
+  </CardWrapper>;
 };
 
 export default LoginForm;
