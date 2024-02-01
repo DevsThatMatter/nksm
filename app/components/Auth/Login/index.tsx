6e4df13f0363f3bcb344606d10b3ebbd50dc96b0
geekysilento <adityakarna.ak@gmail.com>
2024-02-01 18:54:11 +0545

    Added Authentication using Domain ID's and protected routes with middlewares

--- app/components/Auth/Login/index.tsx
+++ app/components/Auth/Login/index.tsx
@@ -3,13 +3,21 @@
 import { Button } from "../../ui/button";
 import { CardWrapper } from "../card-wrapper";
 import { FcGoogle } from 'react-icons/fc'
+import { signIn } from 'next-auth/react';
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+
 const LoginForm = () => {
+  const handleClick = () => {
+    signIn('google', {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+    })
+  }
   return <CardWrapper
     headerLabel="Welcome to NKSM!"
     backButtonLabel="Don't have an account?"
     backButtonHref="/signup"
   >
-    <Button size="lg" className="w-full flex items-center justify-center space-x-3" variant='outline' onClick= {() => {}} >
+    <Button size="lg" className="w-full flex items-center justify-center space-x-3" variant='outline' onClick= {handleClick} >
       <FcGoogle className="h-6 w-6"/>
       <span>Continue with Google</span>
     </Button>
