27717b775aa53394f08977dc210b80c6f9cdf708
Somya <80399932+som-04@users.noreply.github.com>
2024-02-16 20:15:23 +0300

    changes made to form

--- lib/FormSchema/schema.tsx
+++ lib/FormSchema/schema.tsx
@@ -1,17 +1,25 @@
-import { z } from 'zod'
-const ACCEPTED_IMAGE_MIME_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+import { z } from "zod";
+const ACCEPTED_IMAGE_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
 export const FormDataSchema = z.object({
-  iname: z.string().min(1, 'name is required'),
-  
-  condition: z.string().min(1, 'condition is required'),
-  Description: z.string().min(1, 'description is required'),
+  iname: z.string().min(1, "name is required"),
+
+  condition: z.string().min(1, "condition is required"),
+  Description: z.string().min(1, "description is required"),
 
   images: z
     .array(z.any())
     .refine((files) => files.every((file) => file.size <= MAX_FILE_SIZE))
     .refine(
-      (files) => files.every((file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file.type)),
-      'Only .jpg, .jpeg, .png, and .webp formats are supported.'
+      (files) =>
+        files.every((file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file.type)),
+      "Only .jpg, .jpeg, .png, and .webp formats are supported."
     ),
-  })
+
+  Price: z.number().min(0, "Price is required"),
+});
