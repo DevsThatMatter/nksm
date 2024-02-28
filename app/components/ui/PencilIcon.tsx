22474823d3d1287c9069669789c3e6c7b8cf4045
Saarim-Salim <saarimsalim@gmail.com>
2024-02-29 02:13:47 +0300

    Added Edit Icon

---
+++ app/components/ui/PencilIcon.tsx
@@ -0,0 +1,19 @@
+export default function PencilIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
+      <path d="m15 5 4 4" />
+    </svg>
+  );
+}
