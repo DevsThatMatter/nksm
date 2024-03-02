a9b4a0d94cdf575890b5d5d253b912b00be4f2b6
heydoyouknowme0 <akrb2204@gmail.com>
2024-03-02 20:34:42 +0530

    Add to database done

--- app/components/AddListing/Form.tsx
+++ app/components/AddListing/Form.tsx
@@ -37,10 +37,15 @@ import {
   AlertDialogTrigger,
 } from "@/app/components/ui/alert-dialog";
 import { useState } from "react";
+import { Session } from "next-auth";
 
 type Inputs = z.infer<typeof FormDataSchema>;
 
-export default function AddListingForm() {
+export default function AddListingForm({
+  userData,
+}: {
+  userData: Session | null;
+}) {
   const [isPreview, setIsPreview] = useState(false);
   const form = useForm<z.infer<typeof FormDataSchema>>({
     resolver: zodResolver(FormDataSchema),
@@ -65,7 +70,30 @@ export default function AddListingForm() {
   }
 
   async function onSubmit() {
-    console.log(form.getValues());
+    const {
+      iname,
+      quantity,
+      category,
+      description,
+      price,
+      condition,
+      images,
+      negotiate,
+    } = form.getValues();
+    const imagesArray = Object.values(images as Record<string, unknown>).map(
+      (image) => (image as { name: string }).name,
+    );
+    await addProductFromListing({
+      iname,
+      quantity,
+      category,
+      description,
+      price,
+      imagesArray,
+      negotiate,
+      condition,
+      userId: userData?.user?.id,
+    });
   }
 
   return (
--- lib/actions/add-listing.action.ts
+++ lib/actions/add-listing.action.ts
@@ -1,14 +1,44 @@
 "use server";
 
+import mongoose from "mongoose";
+import { connectToDB } from "../database/mongoose";
+import { Product } from "../models/product.model";
+import { User } from "../models/user.model";
+
 type formData = {
+  userId: string | undefined;
   iname: string;
   condition: string;
   description: string;
   category: string;
   price: number | "";
   quantity: number | "";
-  imageArray: [];
+  imagesArray: string[];
+  negotiate: string;
 };
 export async function addProductFromListing(values: formData) {
-  console.log(values);
+  try {
+    await connectToDB();
+    const user = await User.findById(values.userId);
+    const product = await Product.create({
+      Seller: user._id,
+      Total_Quantity_Available: values.quantity,
+      Product_Name: values.iname,
+      Description: values.description,
+      Price: values.price,
+      Images: [
+        "https://img.freepik.com/free-vector/beauty-skin-care-background_52683-728.jpg?t=st=1709385459~exp=1709389059~hmac=183b3e043104f8b588aad1a901df1079015dc73367f17d9cdda3f55d19401e11&w=996",
+      ],
+      Condition: values.condition,
+      Category: values.category,
+      Negotiable: values.negotiate == "yes" ? true : false,
+    });
+
+    await User.findByIdAndUpdate(values.userId, {
+      $push: { products: product._id },
+    });
+    console.log(product);
+  } catch (error) {
+    console.log(error);
+  }
 }
