9b3bec64b737f0162b42759e860429b99c768744
som-04 <somyasati14@gmail.com>
2024-04-05 20:27:02 +0300

    fix: Description knows limits.

--- lib/FormSchema/schema.tsx
+++ lib/FormSchema/schema.tsx
@@ -10,7 +10,10 @@ export const FormDataSchema = z.object({
   iname: z.string().min(1, "Name is required"),
 
   condition: z.string().min(1, "Condition is required"),
-  description: z.string().min(1, "Description is required"),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(1500, "cannot exceed 1500 words"),
   category: z.string().min(1, "Category is required"),
   images: z.array(z.string()).max(6, "Maximum 6 images allowed").nonempty(),
   price: z.coerce.number().min(1, "Price must be atleast 1"),
