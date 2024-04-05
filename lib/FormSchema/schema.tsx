c2401d617b672ca994dd48dbca0c6fe47ac61bb6
som-04 <somyasati14@gmail.com>
2024-04-05 20:39:30 +0300

    fix: limits added to listing fields.

--- lib/FormSchema/schema.tsx
+++ lib/FormSchema/schema.tsx
@@ -7,16 +7,19 @@ const ACCEPTED_IMAGE_MIME_TYPES = [
 ];
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
 export const FormDataSchema = z.object({
-  iname: z.string().min(1, "Name is required"),
+  iname: z.string().min(1, "Name is required").max(30, "can't exceed 30 words"),
 
   condition: z.string().min(1, "Condition is required"),
   description: z
     .string()
     .min(1, "Description is required")
-    .max(1500, "cannot exceed 1500 words"),
+    .max(1500, "can't exceed 1500 words"),
   category: z.string().min(1, "Category is required"),
   images: z.array(z.string()).max(6, "Maximum 6 images allowed").nonempty(),
-  price: z.coerce.number().min(1, "Price must be atleast 1"),
-  quantity: z.coerce.number().min(1, "Quantity must be atleast 1"),
+  price: z.coerce
+    .number()
+    .min(1, "Price must be atleast 1")
+    .max(999999, "can't exceed 999999"),
+  quantity: z.coerce.number().min(1, "Quantity must be at least 1"),
   negotiate: z.string(),
 });
