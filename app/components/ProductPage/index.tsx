f49bfeb1c0392d8db2453c2a5b85211df732af4c
geekysilento <adityakarna.ak@gmail.com>
2024-04-11 01:14:05 +0545

    refactor: made ProductCarousel into a generic

--- app/components/ProductPage/index.tsx
+++ app/components/ProductPage/index.tsx
@@ -1,5 +1,5 @@
 import ImageCard from "./ImageCard";
-import { CarouselItems } from "../HomePage/Carousel";
+import ProductCarousel, { CarouselItems } from "../HomePage/Carousel";
 import ProductDetails from "./ProductDetails";
 import { Product } from "@/types";
 import SellerCard from "./SellerCard";
@@ -11,40 +11,23 @@ import { Carousel, CarouselContent } from "../ui/carousel";
 import { EmblaOptionsType } from "embla-carousel";
 
 const ProductPage = ({ productInfo }: { productInfo: Product }) => {
-  console.log("product info => ", productInfo);
-  const options: EmblaOptionsType = { loop: true, align: "center" };
   return (
     <div key="1" className="mx-auto max-w-full p-4">
       <div className="gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-2">
         <div className="col-span-2 row-span-2">
           <ImageCard images={productInfo.Images} />
           <ProductDetails productInfo={productInfo} />
-        </div>
+          <div className={cn("m-2 mt-8")}>
+            <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
 
-        <div className="col-span-1 row-span-3">
-          <div className="sticky top-[6.3rem] mt-2">
-            <SellerCard
-              sellerInfo={productInfo.Seller}
-              productName={productInfo.Product_Name}
-              productImages={productInfo.Images}
-              productId={productInfo._id}
-            />
-            <CommentCard productId={productInfo._id} />
+            <ProductCarousel productPageCarousel arrows={false} />
           </div>
         </div>
-        <div className={cn("col-span-2 m-2 mt-8")}>
-          <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
-          <Carousel className="w-full" opts={options}>
-            <CarouselContent>
-              <Suspense
-                fallback={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-                  <ProductSkeleton key={i} />
-                ))}
-              >
-                <CarouselItems productPageCarousel />
-              </Suspense>
-            </CarouselContent>
-          </Carousel>
+        <div className="col-span-1">
+          <div className="sticky top-[6.3rem] mt-2">
+            <SellerCard sellerInfo={productInfo.Seller} />
+            <CommentCard productId={productInfo._id} />
+          </div>
         </div>
       </div>
     </div>
