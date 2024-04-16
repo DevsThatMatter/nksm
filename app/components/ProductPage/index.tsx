53d5424cc1b90da70ffe027ba631db5e74f48384
geekysilento <adityakarna.ak@gmail.com>
2024-04-16 16:59:37 +0545

    fix: product page carousel options

--- app/components/ProductPage/index.tsx
+++ app/components/ProductPage/index.tsx
@@ -1,17 +1,12 @@
 import ImageCard from "./ImageCard";
-import ProductCarousel, { CarouselItems } from "../HomePage/Carousel";
+import ProductCarousel from "../HomePage/Carousel";
 import ProductDetails from "./ProductDetails";
 import { Product } from "@/types";
 import SellerCard from "./SellerCard";
 import CommentCard from "./CommentCard";
 import { cn } from "@/app/utils";
-import { Suspense } from "react";
-import ProductSkeleton from "../HomePage/Carousel/ProductSkeleton";
-import { Carousel, CarouselContent } from "../ui/carousel";
-import { EmblaOptionsType } from "embla-carousel";
 
 const ProductPage = ({ productInfo }: { productInfo: Product }) => {
-  const options: EmblaOptionsType = { loop: true, align: "center" };
   return (
     <div key="1" className="mx-auto max-w-full p-4">
       <div className="gap-4 lg:grid lg:grid-cols-3">
@@ -21,11 +16,7 @@ const ProductPage = ({ productInfo }: { productInfo: Product }) => {
           <div className={cn("m-2 mt-8")}>
             <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
 
-            <ProductCarousel
-              productPageCarousel
-              arrows={false}
-              options={options}
-            />
+            <ProductCarousel productPageCarousel arrows={false} />
           </div>
         </div>
         <div className="col-span-1">
