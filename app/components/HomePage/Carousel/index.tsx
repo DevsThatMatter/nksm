32d9d25ad362801aba37cfe221847dee9e54c866
aryansri-19 <aryansri.19@gmail.com>
2024-04-25 17:30:00 +0530

    UI Fixes

--- app/components/HomePage/Carousel/index.tsx
+++ app/components/HomePage/Carousel/index.tsx
@@ -4,34 +4,44 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/app/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-import { EmblaOptionsType, EmblaPluginType } from "embla-carousel";
+import { EmblaOptionsType } from "embla-carousel";
 import ProductCard from "@/app/components/HomePage/Carousel/ProductCard";
 import { fetchRecentProducts } from "@/lib/actions/products.actions";
 import { Suspense } from "react";
 import ProductSkeleton from "./ProductSkeleton";
-import { cn } from "@/app/utils";
 
-const options: EmblaOptionsType = { loop: true, align: "center" };
-
-const ProductCarousel = ({ className }: { className?: string | undefined }) => {
+const ProductCarousel = ({
+  options = {
+    align: "start",
+    dragFree: true,
+    containScroll: "trimSnaps",
+  },
+  productPageCarousel,
+  arrows = true,
+}: {
+  className?: string | undefined;
+  options?: EmblaOptionsType;
+  productPageCarousel?: boolean;
+  arrows?: boolean;
+}) => {
   return (
-    <div className={cn("p-4 lg:m-9 lg:mt-12", className)}>
-      <h1 className="pb-3 text-2xl font-semibold">Recent Items</h1>
-      <Carousel className="w-full" opts={options}>
-        <CarouselContent>
-          <Suspense
-            fallback={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
-              <ProductSkeleton key={i} />
-            ))}
-          >
-            <CarouselItems />
-          </Suspense>
-        </CarouselContent>
-        <CarouselPrevious className="hidden lg:flex" />
-        <CarouselNext className="hidden lg:flex" />
-      </Carousel>
-    </div>
+    <Carousel className="w-full" opts={options}>
+      <CarouselContent>
+        <Suspense
+          fallback={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+            <ProductSkeleton key={i} />
+          ))}
+        >
+          <CarouselItems productPageCarousel={productPageCarousel} />
+        </Suspense>
+      </CarouselContent>
+      {arrows && (
+        <>
+          <CarouselPrevious className="hidden lg:flex" />
+          <CarouselNext className="hidden lg:flex" />
+        </>
+      )}
+    </Carousel>
   );
 };
 
