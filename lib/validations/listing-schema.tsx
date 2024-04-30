02851cdee8a51d562fb0c9cec174301083571747
som-04 <somyasati14@gmail.com>
2024-04-30 10:38:48 +0300

    fix: minor typos

--- lib/validations/listing-schema.tsx
+++ lib/validations/listing-schema.tsx
@@ -1,24 +1,24 @@
 import { z } from "zod";
 const FormDataSchema = z.object({
-  iname: z.string().min(1, "Name is required").max(30, "can't exceed 30 words"),
+  iname: z.string().min(1, "Name is required").max(30, "Can't exceed 30 words"),
 
   condition: z.string().min(1, "Condition is required"),
   description: z
     .string()
     .min(1, "Description is required")
-    .max(1500, "can't exceed 1500 words"),
+    .max(1500, "Can't exceed 1500 words"),
   category: z.string().min(1, "Category is required"),
   price: z.coerce
     .number()
-    .min(1, "Price must be atleast 1")
-    .max(999999, "can't exceed 999999"),
+    .min(1, "Price must be at least 1")
+    .max(999999, "Can't exceed 999999"),
   quantity: z.coerce.number().min(1, "Quantity must be at least 1"),
 });
 export const FormDataSchemaFront = FormDataSchema.extend({
   images: z
     .any()
     .array()
-    .min(1, "Atleast 1 image required")
+    .min(1, "At least 1 image required")
     .max(6, "Maximum 6 images allowed"),
   negotiate: z.string().transform((value) => (value === "Yes" ? true : false)),
 });
@@ -27,7 +27,7 @@ export const FormDataSchemaBack = FormDataSchema.extend({
     .string()
     .url()
     .array()
-    .min(1, "Atleast 1 image required")
+    .min(1, "At least 1 image required")
     .max(6, "Maximum 6 images allowed"),
   negotiate: z.boolean(),
 });
