e0a78369c4260b7500444e01aac944172ede52b2
geekysilento <adityakarna.ak@gmail.com>
2024-05-02 22:04:13 +0530

    fix: reverted incorrect productpage layout

--- app/components/ProductPage/index.tsx
+++ app/components/ProductPage/index.tsx
@@ -1,10 +1,10 @@
-import ImageCard from "./ImageCard";
-import ProductCarousel from "../HomePage/Carousel";
-import ProductDetails from "./ProductDetails";
-import { Product } from "@/types";
-import SellerCard from "./SellerCard";
-import CommentCard from "./CommentCard";
 import { cn } from "@/app/utils";
+import { Product } from "@/types";
+import ProductCarousel from "../HomePage/Carousel";
+import CommentCard from "./CommentCard";
+import ImageCard from "./ImageCard";
+import ProductDetails from "./ProductDetails";
+import SellerCard from "./SellerCard";
 
 const ProductPage = ({ productInfo }: { productInfo: Product }) => {
   return (
@@ -13,13 +13,8 @@ const ProductPage = ({ productInfo }: { productInfo: Product }) => {
         <div className="col-span-2 row-span-2">
           <ImageCard images={productInfo.Images} />
           <ProductDetails productInfo={productInfo} />
-          <div className={cn("m-2 mt-8")}>
-            <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
-
-            <ProductCarousel productPageCarousel arrows={false} />
-          </div>
         </div>
-        <div className="col-span-1">
+        <div className="col-span-1 row-span-3">
           <div className="sticky top-[6.3rem] mt-2">
             <SellerCard
               sellerInfo={productInfo.Seller}
@@ -29,6 +24,11 @@ const ProductPage = ({ productInfo }: { productInfo: Product }) => {
             />
             <CommentCard productId={productInfo._id} />
           </div>
+        </div>
+        <div className={cn("col-span-2 m-2 mt-8")}>
+          <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
+
+          <ProductCarousel productPageCarousel arrows={false} />
         </div>
       </div>
     </div>
