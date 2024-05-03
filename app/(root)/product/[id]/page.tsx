f09df581a7000fb4b05f080c67087dd5e94f1088
geekysilento <adityakarna.ak@gmail.com>
2024-05-03 22:39:23 +0530

    feat: commentskeletons

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCarousel from "@/app/components/HomePage/Carousel";
 import ProductPage, { ProductPageSkeleton } from "@/app/components/ProductPage";
 import { Suspense } from "react";
 
@@ -7,8 +8,16 @@ export default async function Page({
   params: { id: string };
 }) {
   return (
-    <Suspense fallback={<ProductPageSkeleton />} key={"product-page"}>
-      <ProductPage id={id} />
-    </Suspense>
+    <div key="1" className="mx-auto max-w-full p-4">
+      <div className="gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-2">
+        <Suspense fallback={<ProductPageSkeleton />} key={"product-page"}>
+          <ProductPage id={id} />
+        </Suspense>
+        <div className="col-span-2 m-2 mt-8">
+          <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
+          <ProductCarousel productPageCarousel arrows={false} key="carousel" />
+        </div>
+      </div>
+    </div>
   );
 }
--- app/components/ProductPage/index.tsx
+++ app/components/ProductPage/index.tsx
@@ -1,8 +1,9 @@
-import { Suspense } from "react";
 import { fetchProductDetails } from "@/lib/actions/products.actions";
 import { Product } from "@/types";
-import ProductCarousel from "../HomePage/Carousel";
+import { Suspense } from "react";
+
 import CommentCard from "./CommentCard";
+import CommentSkeleton from "./CommentSkeleton";
 import ImageCard, { ImageCardSkeleton } from "./ImageCard";
 import ProductDetails, { ProductDetailsSkeleton } from "./ProductDetails";
 import SellerCard, { SellerCardSkeleton } from "./SellerCard";
@@ -10,68 +11,42 @@ import SellerCard, { SellerCardSkeleton } from "./SellerCard";
 const ProductPage = async ({ id }: { id: string }) => {
   const productInfo: Product = await fetchProductDetails(id);
   return (
-    <div key="1" className="mx-auto max-w-full p-4">
-      <div className="gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-2">
-        <div className="col-span-2 row-span-2">
-          <ImageCard images={productInfo.Images} />
-          <ProductDetails productInfo={productInfo} />
-        </div>
-        <div className="col-span-1 row-span-3">
-          <div className="sticky top-[6.3rem] mt-2">
-            <SellerCard
-              sellerInfo={productInfo.Seller}
-              productName={productInfo.Product_Name}
-              productImages={productInfo.Images}
-              productId={productInfo._id}
-            />
-            <Suspense
-              fallback={
-                <>
-                  <h2 className="mt-6 pb-3 text-2xl font-semibold lg:mt-3 lg:pb-0 lg:text-xl">
-                    Comments
-                  </h2>
-                  <div className="h-full w-full">Loading comments...</div>
-                </>
-              }
-              key="chats"
-            >
-              <CommentCard productId={productInfo._id} />
-            </Suspense>
-          </div>
-        </div>
-        <div className="col-span-2 m-2 mt-8">
-          <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
-          <ProductCarousel productPageCarousel arrows={false} key="carousel" />
+    <>
+      <div className="col-span-2 row-span-2">
+        <ImageCard images={productInfo.Images} />
+        <ProductDetails productInfo={productInfo} />
+      </div>
+      <div className="col-span-1 row-span-3">
+        <div className="sticky top-[6.3rem] mt-2">
+          <SellerCard
+            sellerInfo={productInfo.Seller}
+            productName={productInfo.Product_Name}
+            productImages={productInfo.Images}
+            productId={productInfo._id}
+          />
+          <Suspense fallback={<CommentSkeleton />} key="comments">
+            <CommentCard productId={productInfo._id} />
+          </Suspense>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 export const ProductPageSkeleton = () => {
   return (
-    <div key="1" className="mx-auto max-w-full p-4">
-      <div className="gap-4 lg:grid lg:grid-cols-3 lg:grid-rows-2">
-        <div className="col-span-2 row-span-2">
-          <ImageCardSkeleton />
-          <ProductDetailsSkeleton />
-        </div>
-        <div className="col-span-1">
-          <div className="sticky top-[6.3rem] mt-2">
-            <SellerCardSkeleton />
-
-            <h2 className="mt-6 pb-3 text-2xl font-semibold lg:mt-3 lg:pb-0 lg:text-xl">
-              Comments
-            </h2>
-            <div className="h-full w-full">Loading comments...</div>
-          </div>
-        </div>
-        <div className="col-span-2 m-2 mt-8">
-          <h1 className="pb-3 text-2xl font-semibold">You may also like:</h1>
-          <ProductCarousel productPageCarousel arrows={false} key="carousel" />
+    <>
+      <div className="col-span-2 row-span-2">
+        <ImageCardSkeleton />
+        <ProductDetailsSkeleton />
+      </div>
+      <div className="col-span-1">
+        <div className="sticky top-[6.3rem] mt-2">
+          <SellerCardSkeleton />
+          <CommentSkeleton />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
