41144a5ab9216d110f0b84e8bf2ae0dd71d5be78
antri <akrb2204@gmail.com>
2024-05-09 17:56:47 +0530

    style: new email template

--- lib/actions/email.actions.ts
+++ lib/actions/email.actions.ts
@@ -32,91 +32,85 @@ export async function sendEmail(
   },
 ) {
   const { price } = formData;
+  const inviteLink = `https://nksm.vercel.app/product/${productId}`;
 
   const htmlTemplate = `
-  <!doctype html>
-  <html lang="en">
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html dir="ltr" lang="en">
+  
     <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Bid Offer</title>
+      <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+      <meta name="color-scheme" content="light dark" />
+      <meta name="supported-color-schemes" content="light dark" />
+      <style>
+        @media(prefers-color-scheme:dark) {
+          .dark_bg-white {
+            background-color: rgb(255, 255, 255) !important
+          }
+  
+          .dark_text-_AAAAAA {
+            color: rgb(170, 170, 170) !important
+          }
+  
+          .dark_text-black {
+            color: rgb(0, 0, 0) !important
+          }
+  
+          .dark_text-blue-700 {
+            color: rgb(29, 78, 216) !important
+          }
+  
+          .dark_invert {
+            filter: invert(100%) !important
+          }
+        }
+      </style>
     </head>
-    <body
-      style="margin: 0; font-family: Arial, sans-serif; background-color: #f8f8f8"
-    >
-      <main
-        style="
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          min-height: 100vh;
-        "
-      >
-        <div
-          style="
-            width: 90%;
-            max-width: 800px;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 16px;
-            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-          "
-        >
-          <h1 style="color: indigo; margin-bottom: 20px; text-align: center">
-            Bid Offer
-          </h1>
-          <p style="margin-bottom: 30px; text-align: center">
-            You've received a bid offer from ${senderEmail}.
-          </p>
-          <section
-            style="
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              margin-top: 20px;
-            "
-          >
-            <h2 style="margin-bottom: 10px">${productName}</h2>
-            <img
-              src={${productImages[0]}}
-              alt="Product"
-              style="max-width: 100%; height: auto; margin-bottom: 20px"
-            />
-            <div
-              style="display: flex; flex-direction: column; align-items: center"
-            >
-              <div style="margin-bottom: 10px">
-                <span style="font-weight: bold">Bid:</span>
-                <span
-                  style="
-                    font-size: 24px;
-                    font-weight: bold;
-                    color: #007bff;
-                    margin-left: 5px;
-                  "
-                  >${price}</span
-                >
-              </div>
-              <button
-                style="
-                  padding: 10px 20px;
-                  background-color: #007bff;
-                  color: #fff;
-                  border: none;
-                  border-radius: 8px;
-                  cursor: pointer;
-                  font-size: 24px;
-                "
-              >
-                <a style="color: #fff" href="">Visit NKSM</a>
-              </button>
-            </div>
-          </section>
-        </div>
-      </main>
+    <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Visit NKSM to view the offer
+    </div>
+  
+    <body style="margin-left:auto;margin-right:auto;margin-top:auto;margin-bottom:auto;padding-left:0.5rem;padding-right:0.5rem;font-family:ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;">
+      <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:550px;margin-left:auto;margin-right:auto;margin-top:40px;margin-bottom:40px;border-radius:0.5rem;border-width:1px;border-style:solid;border-color:rgb(234,234,234);padding:20px">
+        <tbody>
+          <tr style="width:100%">
+            <td>
+              <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-bottom:1.25rem;margin-top:1.75rem">
+                <tbody>
+                  <tr>
+                    <td><img class="dark_invert" alt="NKSM" height="90" src="https://pub-2188c30af1504cb0a1acb7e6f7e0efae.r2.dev/logon.png" style="display:block;outline:none;border:none;text-decoration:none;margin-left:auto;margin-right:auto;margin-top:0px;margin-bottom:0px" width="200" /></td>
+                  </tr>
+                </tbody>
+              </table>
+              <p style="font-size:2.25rem;line-height:2.5rem;margin:16px 0;margin-bottom:0.25rem;font-weight:600">Bid Offer</p>
+              <p style="font-size:1rem;line-height:1.5rem;margin:16px 0;margin-top:0.25rem">${senderEmail}<!-- --> has sent you an offer.</p>
+              <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+            
+                  <tr>
+                    <td><img alt="Product Image" src="${productImages[0]}" style="display:block;outline:none;border:none;text-decoration:none;margin-bottom:0.5rem;aspect-ratio:4/3;width:100%;border-radius:0.375rem" />
+                      <p style="font-size:1.875rem;line-height:2.25rem;margin:16px 0;margin-bottom:0px;margin-top:0.5rem;font-weight:500">${productName}</p>
+                      <p style="font-size:1.25rem;line-height:1.75rem;margin:16px 0;margin-top:0px;margin-bottom:0px">₹ <!-- -->${price}</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-top:2rem;margin-bottom:2rem;text-align:center">
+                <tbody>
+                  <tr>
+                    <td><a class="dark_bg-white dark_text-black" href="${inviteLink}" style="line-height:1.25rem;text-decoration:none;display:inline-block;max-width:100%;border-radius:0.25rem;background-color:rgb(0,0,0);padding-left:1.25rem;padding-right:1.25rem;padding-top:0.75rem;padding-bottom:0.75rem;text-align:center;font-size:0.875rem;font-weight:600;color:rgb(255,255,255);text-decoration-line:none;padding:12px 20px 12px 20px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%;mso-text-raise:18" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px">Visit NKSM</span><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a></td>
+                  </tr>
+                </tbody>
+              </table>
+              <p style="font-size:14px;line-height:24px;margin:16px 0">or copy and paste this URL into your browser:<!-- --> <a href="https://nksm.vercel.app" class="dark_text-blue-700" style="color:rgb(37,99,235);text-decoration:none;text-decoration-line:none" target="_blank">https://nksm.vercel.app</a></p>
+              <hr style="width:100%;border:none;border-top:1px solid #eaeaea;margin-left:0px;margin-right:0px;margin-top:1.5rem;margin-bottom:1.5rem;border-width:1px;border-style:solid;border-color:rgb(234,234,234)" />
+              <p class="dark_text-_AAAAAA" style="font-size:0.75rem;line-height:1rem;margin:16px 0;color:rgb(102,102,102)">If you were not expecting this invitation, you can ignore this email. If you are concerned about your account&#x27;s safety, please reply to this email to get in touch with us.</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </body>
-  </html>  
-  `;
+  
+  </html>
+    `;
   try {
     await connectToDB();
     if (senderEmail === receiverEmail) {
@@ -142,6 +136,7 @@ export async function sendEmail(
 
     await transporter.sendMail({
       from: `"NKSM" <${senderEmail}>`,
+      subject: `You've received a bid offer from ${senderEmail.substring(0, senderEmail.indexOf("@"))}`,
       to: receiverEmail,
       html: htmlTemplate,
     });
